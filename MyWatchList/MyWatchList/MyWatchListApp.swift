6ce02f12da173c238f6f8b98fdb7cddf65be0a31
mlle-alois <alois.zimmermann45@gmail.com>
2022-06-28 17:54:14 +0200

    INIT

---
+++ MyWatchList/MyWatchList/MyWatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWatchListApp.swift
+//  MyWatchList
+//
+//  Created by Etudiant on 28/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyWatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
