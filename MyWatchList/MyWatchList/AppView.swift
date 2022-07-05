84e96d874148dc2768dba5d6083a09525e67f296
mlle-alois <alois.zimmermann45@gmail.com>
2022-07-05 19:00:14 +0200

    MAIN

---
+++ MyWatchList/MyWatchList/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  MyWatchList
+//
+//  Created by Etudiant on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppView: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
