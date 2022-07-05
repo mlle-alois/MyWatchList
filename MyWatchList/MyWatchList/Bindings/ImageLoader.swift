f1ebfdb61cba4fd429b8f806bed86a27233e277f
mlle-alois <alois.zimmermann45@gmail.com>
2022-07-05 19:03:53 +0200

    REFACTO

--- MyWatchList/MyWatchList/Bindings/ImageLoader.swift
+++ MyWatchList/MyWatchList/Bindings/ImageLoader.swift
@@ -7,21 +7,12 @@
 
 import SwiftUI
 
-private let _imageCache = NSCache<AnyObject, AnyObject>()
-
 class ImageLoader: ObservableObject {
     
     @Published var image: UIImage?
     @Published var isLoading = false
-    
-    var imageCache = _imageCache
 
     func loadImage(with url: URL) {
-        let urlString = url.absoluteString
-        if let imageFromCache = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
-            self.image = imageFromCache
-            return
-        }
         
         DispatchQueue.global(qos: .background).async { [weak self] in
             guard let self = self else { return }
@@ -30,7 +21,6 @@ class ImageLoader: ObservableObject {
                 guard let image = UIImage(data: data) else {
                     return
                 }
-                self.imageCache.setObject(image, forKey: urlString as AnyObject)
                 DispatchQueue.main.async { [weak self] in
                     self?.image = image
                 }
